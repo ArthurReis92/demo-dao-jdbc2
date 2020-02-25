cfb56729fa96452ebacf6a7f3c680433abee99b5
Arthur Reis <afelipereis@gmail.com>
2020-02-24 22:11:56 -0300

    findById DepartmentDaoJDBC implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -8,9 +8,11 @@ public class Program2 {
 
 	public static void main(String[] args) {
 
-		Department department = new Department(21, "Music");
-		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TEST 1: department findById ===");
+		
+		Department department = departmentDao.findById(1);
 		
 		System.out.println(department);
 	}
